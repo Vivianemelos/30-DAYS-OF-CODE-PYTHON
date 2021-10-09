d496a377cd1edf231de11def057b1b94bb76b383
Viviane Sousa de Melo <vivianemeloscc@gmail.com>
2021-10-09 02:47:39 -0300

    Exercicio em python3

---
+++ day12.py
@@ -0,0 +1,36 @@
+
+
+class Student(Person):
+    #   Class Constructor
+    #   
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    def __init__(self, firstName, lastName, idNumber, scores):
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def calculate(self):
+        s = sum(self.scores)/len(self.scores)
+        if s>=90:
+            return 'O'
+        elif s>=80:
+            return 'E'
+        elif s>=70:
+            return 'A'
+        elif s>=55:
+            return 'P'
+        elif s>=40:
+            return 'D'
+        else:
+            return 'T'
+            
+        
+            
