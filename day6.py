2cae29e2d133cba08fee34dbd831b4cc80543a0e
Viviane Sousa de Melo <vivianemeloscc@gmail.com>
2022-01-31 01:22:19 -0300

    Nova versão do exercício de numero 6.

--- day6.py
+++ day6.py
@@ -3,3 +3,11 @@ n = int(input())
 for i in range(n):
     palavra = input()
     print(palavra[::2] + " " + palavra[1::2])
+    
+    ''' Nova versão '''
+    
+if __name__ == '__main__':
+    N = int(input().strip())
+    for _ in range(N):
+        palavra = input()
+        print(palavra[::2] + " " + palavra[1::2])
